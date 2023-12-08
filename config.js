c964038d03835af3c94704ab3c2e70294a3701b1
Abdul Rasheed Azar <rasheedazar88@gmail.com>
2023-12-08 18:10:25 +0530

    updated

---
+++ config.js
@@ -0,0 +1,34 @@
+const dataConfig = [
+    {
+        thumbnail: 'img1.jpg',
+        title: 'Pourquoi nous choisir ?',
+        stories: [
+            "https://cdn.shopify.com/videos/c/vp/79fd44ce08c64d9f85149653422b8e1d/79fd44ce08c64d9f85149653422b8e1d.HD-1080p-7.2Mbps-19337928.mp4"
+        ]
+    },
+    {
+        thumbnail: 'img2.png',
+        title: 'Plus confortables ? 🧵',
+        stories: [
+            "https://cdn.shopify.com/videos/c/vp/08afc4eb43724fc393e723512f4d2630/08afc4eb43724fc393e723512f4d2630.HD-1080p-7.2Mbps-19338787.mp4"
+        ]
+    },
+    {
+        thumbnail: 'img3.png',
+        title: 'Solide comme un roc 💪',
+        stories: [
+            "https://cdn.shopify.com/videos/c/vp/4db7b032ddca47a8ace6a51c84b2aa68/4db7b032ddca47a8ace6a51c84b2aa68.HD-1080p-7.2Mbps-19338842.mp4"
+        ]
+    },
+    {
+        thumbnail: 'img4.jpg',
+        title: 'Vos Outfits ❤️',
+        stories: [
+            "https://cdn.shopify.com/videos/c/vp/e45d57071f40414bbffb22a820c0ff57/e45d57071f40414bbffb22a820c0ff57.HD-1080p-7.2Mbps-19751821.mp4",
+            "https://cdn.shopify.com/videos/c/vp/643c52f231b04a2bb6f8ea3dde1d4ef0/643c52f231b04a2bb6f8ea3dde1d4ef0.HD-1080p-7.2Mbps-19752008.mp4",
+            "https://cdn.shopify.com/videos/c/vp/59fea322f952442fa51cac51c350bf6f/59fea322f952442fa51cac51c350bf6f.HD-1080p-7.2Mbps-19752158.mp4",
+            "https://cdn.shopify.com/videos/c/vp/68cceeec72524dbc913ae61a086652db/68cceeec72524dbc913ae61a086652db.HD-1080p-7.2Mbps-19752380.mp4",
+            "https://cdn.shopify.com/videos/c/vp/1d4699716fe348b69d8f7b78dd264796/1d4699716fe348b69d8f7b78dd264796.HD-1080p-7.2Mbps-19752701.mp4"
+        ]
+    }
+]
